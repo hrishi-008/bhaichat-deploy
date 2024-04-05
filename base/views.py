8aea5ad55f2f37f9a749bd2f4ebc59667f0a5ad2
Hrishikesh Kalola <97169623+hrishi-008@users.noreply.github.com>
2024-04-05 11:19:00 +0530

    Update views.py
    added creds

--- base/views.py
+++ base/views.py
@@ -8,8 +8,8 @@ from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def getToken(request):
-    appId = "f86e8e7143f14315b01a90cf2f3f0b64";
-    appCertificate = 'b267ecd093c946e0a8e4a8afda6918c9'
+    appId = "3ba02817229d40a5a4564b825b831430";
+    appCertificate = 'f6bc357666f44d6394388b8cecf50d3a'
     channelName = request.GET.get('channel')
     uid = random.randint(1, 230)
     expireTimeInSeconds = 3600*24
